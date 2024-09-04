8efa9cdf2db34c94011f205e804cb82afabfb771
Franleiva <franfaha54@gmail.com>
2024-09-04 19:38:52 -0300

    fran-start-a project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,13 @@
 // app/_layout.tsx
 import React from 'react';
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const RootLayout = () => {
   return (
-    <SafeAreaProvider>
-      {/* El Slot representa donde se renderizan los sub-layouts, como (tabs)/_layout.tsx */}
-      <Slot />
-    </SafeAreaProvider>
+   <Stack>
+    
+   </Stack>
   );
 };
 
